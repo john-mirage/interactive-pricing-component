ef3a91c29ee7e066abe3a6dd2c530c8e61efdc7b
john-mirage <chaosdynamix@posteo.net>
2022-03-28 13:54:20 +0200

    feat: create vitejs project with react-ts template

---
+++ src/components/page.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  padding: 50px;
+  background-color: #fafafa;
+`;
+
+function Page() {
+  return (
+    <Container />
+  )
+}
+
+export default Page
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react({
+    babel: {
+      plugins: ["babel-plugin-styled-components"],
+    },
+  })],
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, 'src/components'),
+      '@assets': resolve(__dirname, 'src/assets'),
+    },
+  },
+})
