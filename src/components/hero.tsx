f085ea655ed7c20adc2da3b63906e36e0f44217c
john-mirage <chaosdynamix@posteo.net>
2022-03-30 00:50:25 +0200

    ref: rework hero image

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,26 +4,21 @@ import Circles from '@assets/images/pattern-circles.svg';
 const Container = styled.section`
     position: relative;
     z-index: 50;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     width: 100%;
-    height: auto;
-    padding-top: 3.2rem;
-    padding-bottom: 3.2rem;
+    min-height: 14.5rem;
     text-align: center;
     margin-bottom: 3.2rem;
+    background-image: url(${Circles});
+    background-repeat: no-repeat;
+    background-size: contain;
+    background-position: center;
 
     @media screen and (min-width: ${props => props.theme.screen.md}) {
         margin-bottom: 5.4rem;
     }
-`;
-
-const Image = styled.img`
-    position: absolute;
-    z-index: 10;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 14.6rem;
-    height: 14.5rem;
 `;
 
 const Title = styled.h1`
@@ -44,7 +39,7 @@ const Title = styled.h1`
 const Subtitle = styled.p`
     position: relative;
     z-index: 20;
-    width: 18rem;
+    max-width: 18rem;
     margin-left: auto;
     margin-right: auto;
     font-size: 1.3rem;
@@ -53,7 +48,7 @@ const Subtitle = styled.p`
     color: ${props => props.theme.color.neutral.grayishBlue};
 
     @media screen and (min-width: ${props => props.theme.screen.md}) {
-        width: auto;
+        max-width: none;
         font-size: 1.5rem;
     }
 `;
@@ -61,7 +56,6 @@ const Subtitle = styled.p`
 function Hero() {
     return (
         <Container>
-            <Image src={Circles} alt="Circles pattern illustration" />
             <Title>Simple, traffic-based pricing</Title>
             <Subtitle>Sign-up for our 30-day trial. No credit card required.</Subtitle>
         </Container>
