80e193313c9d1d3e163444e2cd1abfdbb178f4ca
john-mirage <chaosdynamix@posteo.net>
2022-03-30 02:38:09 +0200

    feat: add github action and vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/interactive-pricing-component/",
   plugins: [react({
     babel: {
       plugins: ["babel-plugin-styled-components"],
